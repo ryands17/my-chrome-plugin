c7db7af758af62db971b360efd4ebfedadab6d32
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2019-01-16 00:36:26 +0530

    fixed package vulnerability

---
+++ public/background.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+;(function() {
+  chrome.runtime.onInstalled.addListener(function(details) {
+    if (details && details.reason && details.reason == 'install')
+      chrome.tabs.create({ url: 'index.html' })
+  })
+
+  chrome.browserAction.onClicked.addListener(function(tab) {
+    // chrome.tabs.create({ url: "index.html" });
+  })
+})()
--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import AnalogClock, { Themes } from 'react-analog-clock'
 
 class App extends Component {
+  width = 180
+  state = {
+    timezones: [
+      {
+        id: 1,
+        name: 'India',
+        offset: '5.5'
+      },
+      {
+        id: 2,
+        name: 'US',
+        offset: '-5'
+      },
+      {
+        id: 3,
+        name: 'UK',
+        offset: '1'
+      }
+    ]
+  }
+
   render() {
+    const { timezones } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Fragment>
+        <div className="bg-class" />
+        <div className="App_main">
+          <h1 style={{ color: '#fff' }}>Your timezones</h1>
+          <div className="App_timezone">
+            {timezones.map(({ id, offset, name }) => (
+              <div key={id} className="App_clock">
+                <AnalogClock
+                  gmtOffset={offset}
+                  width={this.width}
+                  theme={Themes.light}
+                />
+                <h3 className="App_text">{name}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Fragment>
+    )
   }
 }
 
-export default App;
+export default App
